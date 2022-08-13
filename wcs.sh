3b0df526dca84a93056066c9c6cf4f4a1f86c6a0
Bhood23 <80179069+Bhood23@users.noreply.github.com>
2022-08-13 10:43:17 +1000

    Update wcs.sh

--- wcs.sh
+++ wcs.sh
@@ -9,6 +9,15 @@ echo Installing vncserver and xfce4...
 sudo apt update
 sudo apt install tigervnc-standalone-server tigervnc-common xfce4 xfce4-terminal xfce4-taskmanager dbus-x11 --no-install-recommends -y
 
+!pip install git+https://github.com/demotomohiro/remocolab.git
+import remocolab
+
+#IF YOU WANT SSH ONLY 
+#remocolab.setupSSHD(ngrok_region = "ap")
+
+#IF YOU WANT SSH AND VNC(GUI)
+remocolab.setupVNC(ngrok_region = "ap")
+
 # Setup wine apt repo
 echo Adding wine repo...
 sudo dpkg --add-architecture i386
@@ -31,6 +40,8 @@ rm ngrok-v3-stable-linux-amd64.tgz # Clean up
 echo Now go to https://dashboard.ngrok.com/get-started/setup and find your token in Step 2 Fork This Repo/Script And Edit It Change "auth token" to your authtoken to "Enter auth"
 export token="enter auth"
 
+
+
 # Check if the user actually copy the whole add-authtoken command or just the token itself
 if [[ $token == *"add-authtoken"* ]]; then
     $token
@@ -39,7 +50,6 @@ else
 fi
 
 # Start vncserver
-vncserver
 export DISPLAY=:1
 xfce4-session &
 
@@ -47,7 +57,6 @@ xfce4-session &
 # Google Cloud Shell has a really small /home parition (5GB), so you might need to change the WINEPREFIX to somewhere else
 echo Google Cloud Shell has a small partition of 5GB, would you wish to set WINEPREFIX to /tmp, which has much more space?
 echo Please note that data outside $HOME will be deleted after each Google Cloud Shell session.
-
 
 if [ "$answer" != "${answer#[Yy]}" ] ; then
     mkdir /tmp/wineprefix
